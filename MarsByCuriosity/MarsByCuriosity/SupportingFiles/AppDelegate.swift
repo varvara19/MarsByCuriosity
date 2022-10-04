2ac8fac6facd8a099977c00f54656af376ac6bfb
Varvara <homelyfill@gmail.com>
2022-10-04 12:25:54 +0300

    Create project, update gitignore

---
+++ MarsByCuriosity/MarsByCuriosity/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MarsByCuriosity
+//
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
---
+++ MarsByCuriosity/MarsByCuriosity/UI/RootViewController/RootViewController.swift
@@ -0,0 +1,68 @@
+//
+//  RootViewController.swift
+//  MarsByCuriosity
+//
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    private var childViewController: UIViewController?
+    
+    static var shared: RootViewController? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.window?.rootViewController as? RootViewController
+        }
+        return nil
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        animationToDashboard()
+    }
+    
+    private func animationToDashboard() { }
+    
+    private func addNewChildViewController(vc: UIViewController) {
+        self.animation(duration: 0.26, newVC: vc, oldVC: self.childViewController)
+        self.childViewController = vc
+    }
+    
+    private func animation(duration: TimeInterval, newVC: UIViewController, oldVC: UIViewController? = nil) {
+        
+        if let oldVC = oldVC {
+            self.addChild(newVC)
+            self.view.insertSubview(newVC.view, belowSubview: oldVC.view)
+            
+            UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut) {
+                newVC.didMove(toParent: self)
+                oldVC.view.alpha = 0.2
+                newVC.view.alpha = 1
+            } completion: { finished in
+                oldVC.willMove(toParent: nil)
+                oldVC.view.removeFromSuperview()
+                oldVC.removeFromParent()
+            }
+        } else {
+            self.addChild(newVC)
+            self.view.addSubview(newVC.view)
+            newVC.didMove(toParent: self)
+        }
+    }
+    
+    func topViewController() -> UIViewController? {
+        return self.childViewController
+    }
+    
+    func topNavigationController() -> UINavigationController? {
+        var topNavigationController: UINavigationController?
+        
+        switch self.topViewController() {
+            case let controller as UINavigationController: topNavigationController = controller
+            default: break
+        }
+        return topNavigationController
+    }
+}
+
